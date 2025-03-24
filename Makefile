60af0638db36b386ad1528654474ec4803b40bb9
Eric Southam <eric.southam@lvt.com>
2025-03-24 13:15:09 -0600

    chore: update CMake build environment for debugging from macos and clion docs: update README.md chore: update Makefile to include macos folders (does not hurt linux) chore: update vscode workspace launch settings chore: update .gitignore for CMake output build folder

--- Makefile
+++ Makefile
@@ -1,11 +1,12 @@
 CFLAGS = -Wall -O2 -s -Werror
 LDFLAGS = -lusb -lftdi1 -s
 PROG = ft232r_cbus
+INCLUDES = -I/usr/include/ -I/opt/homebrew/include/
 
 all:	$(PROG)
 
 $(PROG):	main.c
-	$(CC) $(CFLAGS) -o $@ $< $(LDFLAGS)
+	$(CC) $(CFLAGS) -o $@ $< $(LDFLAGS) -I $(INCLUDES)
 
 clean:
 	rm -f $(PROG)
--- main.c
+++ main.c
@@ -1,47 +1,51 @@
-/*  bitbang_cbus.c
+/*  main.c
+ *  modified from example/eeprom_bitbang.c
+ *  Thanks to Steve Brown <sbrown@ewol.com> for the information how to do it.
 
-    Example to use CBUS bitbang mode of newer chipsets.
-    You must enable CBUS bitbang mode in the EEPROM first.
-
-    Thanks to Steve Brown <sbrown@ewol.com> for the
-    the information how to do it.
-
-    The top nibble controls input/output and the bottom nibble
-    controls the state of the lines set to output. The datasheet isn't clear
-    what happens if you set a bit in the output register when that line is
-    conditioned for input. This is described in more detail
-    in the FT232R bitbang app note.
-
-    BITMASK
-    CBUS Bits
-    3210 3210
-    xxxx xxxx
-    |    |------ Output Control 0->LO, 1->HI
-    |----------- Input/Output   0->Input, 1->Output
-
-    Example:
-    All pins to output with 0 bit high: 0xF1 (11110001)
-    Bits 0 and 1 to input, 2 and 3 to output and masked high: 0xCC (11001100)
-
-    0x8C turn cbus[3] high by itself
-    0x4C turn cbus[2] high by itself
-
-    The input is standard "0x" hex notation.
-    A carriage return terminates the program.
-
-    This program is distributed under the GPL, version 2
-*/
+ *  The top nibble controls input/output
+ *  the bottom nibble controls the state of the lines set to output.
+ *  The datasheet isn't clear what happens if you set a bit
+ *  in the output register when that line is conditioned as input.
+ *  This is described in more detail in the FT232R bitbang app note.
+ *
+ *  --BITMASK--
+ *  CBUS Bits
+ *  3210 3210
+ *  xxxx xxxx
+ *      |----|  Set Output Control [0->LO, 1->HI]
+ * |----|       Cfg  Input/Output  [0->Input, 1->Output]
+ *
+ *  Example:
+ *  All pins to output with 0 bit high: 0xF1 (11110001)
+ *  Bits 0 and 1 to input, 2 and 3 to output and masked high: 0xCC (11001100)
+ *      0x8C turn cbus[3] high by itself
+ *      0x4C turn cbus[2] high by itself
+ *
+ *  The input is standard "0x" hex notation.
+ *  A carriage return terminates the program.
+ *
+ *  This program is distributed under the GPL, version 2
+ *
+ *  SEQUENCE: Enter BootLoader Mode: Cbus[3]=BOOT0, Cbus[2]=RST (ignore CBUS1 and CBUS0 as inputs)
+ *  Note: Cbus[2]=RST drives an inversion of uC-nRst
+ *  0xCC - BOOT0-High and RST-High  = BootLoader-Mode in Reset
+ *  0xC8 - BOOT0-High, RST-Low      = Bootloader-Mode RUN
+ *  0x00 - CBUS all inputs          = Continue running (Reset-PullUp, Boot0-pullDown)
+ *  ----
+ *  0xC4 - BOOT0-LOW, RST-High     = Normal-Mode - in Reset
+ *  0xC0 - BOOT0-LOW, RST-LOW       = Normal-Mode - RUN (pins driven)
+ *  0x00 - CBUS all inputs          = Continue running (Reset-PullUp, Boot0-pullDown)
+ */
 
 #include <stdio.h>
 #include <unistd.h>
 #include <stdlib.h>
 #include <libftdi1/ftdi.h>    // ref ftdi version 1.5-x. "sudo apt-get install libftdi1-dev"
-//#include <ftdi1/ftdi.h>     // USE for mac // ref ftdi version 1.5-x. "sudo apt-get install libftdi1-dev"
 
 int main(void)
 {
     struct ftdi_context *ftdi;
-    int f;
+    int err;
     unsigned char buf[1];
     unsigned char bitmask;
     char input[10];
@@ -53,14 +57,14 @@ int main(void)
     }
     ftdi->module_detach_mode = AUTO_DETACH_REATACH_SIO_MODULE;    // Set this to avoid kernel module already attached error and reattach it after use
 
-    f = ftdi_usb_open_desc(ftdi, 0x0403, 0x6001, "FT232R USB UART", NULL);    // Use Description specific string(default for chip used on board)
-    if (f < 0 && f != -5)
+    err = ftdi_usb_open_desc(ftdi, 0x0403, 0x6001, "FT232R USB UART", NULL);    // Use Description specific string(default for chip used on board)
+    if (err < 0 && err != -5)
     {
-        fprintf(stderr, "unable to open ftdi device: %d (%s)\n", f, ftdi_get_error_string(ftdi));
+        fprintf(stderr, "unable to open ftdi device: %d (%s)\n", err, ftdi_get_error_string(ftdi));
         ftdi_free(ftdi);
         exit(-1);
     }
-    printf("ftdi open succeeded: %d\n",f);
+    printf("ftdi open succeeded: %d\n", err);
 
     while (1)
     {
@@ -70,20 +74,20 @@ int main(void)
             break;
         bitmask = strtol(input, NULL, 0);
         printf("Using bitmask 0x%02x\n", bitmask);
-        f = ftdi_set_bitmode(ftdi, bitmask, BITMODE_CBUS);
-        if (f < 0)
+        err = ftdi_set_bitmode(ftdi, bitmask, BITMODE_CBUS);
+        if (err < 0)
         {
-            fprintf(stderr, "set_bitmode failed for 0x%x, error %d (%s)\n", bitmask, f, ftdi_get_error_string(ftdi));
+            fprintf(stderr, "set_bitmode failed for 0x%x, error %d (%s)\n", bitmask, err, ftdi_get_error_string(ftdi));
             ftdi_usb_close(ftdi);
             ftdi_free(ftdi);
             exit(-1);
         }
 
         // read CBUS
-        f = ftdi_read_pins(ftdi, &buf[0]);
-        if (f < 0)
+        err = ftdi_read_pins(ftdi, &buf[0]);
+        if (err < 0)
         {
-            fprintf(stderr, "read_pins failed, error %d (%s)\n", f, ftdi_get_error_string(ftdi));
+            fprintf(stderr, "read_pins failed, error %d (%s)\n", err, ftdi_get_error_string(ftdi));
             ftdi_usb_close(ftdi);
             ftdi_free(ftdi);
             exit(-1);
